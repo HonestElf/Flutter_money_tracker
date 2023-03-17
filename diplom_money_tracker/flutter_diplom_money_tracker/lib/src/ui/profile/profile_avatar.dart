98c957910d454bb0bd9af6ff363c2c1d61233272
Vasiliy Prokudin <vd.prokudin@jet.su>
2023-03-17 17:45:13 +0300

    Added firebase storage to avatar component

--- diplom_money_tracker/flutter_diplom_money_tracker/lib/src/ui/profile/profile_avatar.dart
+++ diplom_money_tracker/flutter_diplom_money_tracker/lib/src/ui/profile/profile_avatar.dart
@@ -1,59 +1,91 @@
 import 'dart:io';
+import 'dart:math';
 
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 
 class ProfileAvatar extends StatefulWidget {
-  const ProfileAvatar({super.key});
+  const ProfileAvatar({super.key, required this.currentUser});
+
+  final User currentUser;
 
   @override
   State<ProfileAvatar> createState() => _ProfileAvatarState();
 }
 
 class _ProfileAvatarState extends State<ProfileAvatar> {
-  late XFile? _avatarImage;
+  final storage = FirebaseStorage.instance;
   final ImagePicker _picker = ImagePicker();
+
+  String? _imageUrl;
 
   @override
   void initState() {
     super.initState();
-    try {
-      _avatarImage = XFile('assets/images/altAvatar.jpg');
-    } catch (e) {
-      ScaffoldMessenger.of(context).showSnackBar(
-        const SnackBar(
-            duration: Duration(milliseconds: 1000),
-            backgroundColor: Colors.red,
-            content: Text('Ошибка загрузки изображения')),
-      );
-    }
+
+    _loadUserAvatar();
   }
 
   void picImageFromGallery() async {
     final chosenImage = await _picker.pickImage(source: ImageSource.gallery);
 
+    final imgRef =
+        storage.ref().child('avatars/${widget.currentUser.uid}_avatar');
+
     if (chosenImage != null) {
+      imgRef.putFile(File(chosenImage.path));
+      _loadUserAvatar();
+    }
+  }
+
+  void _showError(String errorMessage) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+          duration: const Duration(milliseconds: 2000),
+          backgroundColor: Colors.red,
+          content: Text(errorMessage)),
+    );
+  }
+
+  void _loadUserAvatar() async {
+    try {
+      final imageUrl = await storage
+          .ref('avatars/${widget.currentUser.uid}_avatar')
+          .getDownloadURL();
+
       setState(() {
-        _avatarImage = chosenImage;
+        _imageUrl = imageUrl;
       });
+    } on FirebaseException catch (fireError) {
+      setState(() {
+        _imageUrl = null;
+      });
+
+      if (fireError.code == 'object-not-found') {
+        _showError('Аватар не найден');
+      }
+    } catch (error) {
+      setState(() {
+        _imageUrl = null;
+      });
+      _showError('Ошибка загрузки изображения');
     }
   }
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: () {
-        // picImageFromGallery();
-      },
-      child: CircleAvatar(
+        onTap: () {
+          picImageFromGallery();
+        },
+        child: CircleAvatar(
           radius: 40,
-          backgroundImage: const AssetImage('assets/images/defaultAvatar.png')
-
-          // backgroundImage:_avatarImage != null
-          //     ? FileImage(File('assets/images/altAvatar.jpg'))
-          //     : const AssetImage('assets/images/defaultAvatar.png')
-          // as ImageProvider),
-          ),
-    );
+          backgroundImage: _imageUrl != null
+              ? NetworkImage(_imageUrl!)
+              : const AssetImage('assets/images/altAvatar.jpg')
+                  as ImageProvider,
+        ));
   }
 }
--- diplom_money_tracker/flutter_diplom_money_tracker/lib/src/ui/profile/profile_view.dart
+++ diplom_money_tracker/flutter_diplom_money_tracker/lib/src/ui/profile/profile_view.dart
@@ -26,42 +26,44 @@ class _ProfileViewState extends State<ProfileView> {
       appBar: AppBar(
         backgroundColor: const Color(0xFF9053EB),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(25),
-        child: Row(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const ProfileAvatar(),
-            const SizedBox(
-              width: 20,
-            ),
-            Column(
-              children: [
-                Text(
-                  _currentUser != null
-                      ? _currentUser!.email!
-                      : 'user not found',
-                  style: TextStyle(fontSize: 17),
-                ),
-                const SizedBox(
-                  height: 15,
-                ),
-                ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                        backgroundColor: const Color(0xFF9053EB),
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(15)),
-                        minimumSize: const Size(150, 50)),
-                    onPressed: firebaseLogout,
-                    child: const Text(
-                      'Выйти',
-                      style: TextStyle(fontSize: 17),
-                    )),
-              ],
+      body: _currentUser != null
+          ? Padding(
+              padding: const EdgeInsets.all(25),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  ProfileAvatar(currentUser: _currentUser!),
+                  const SizedBox(
+                    width: 20,
+                  ),
+                  Column(
+                    children: [
+                      Text(
+                        _currentUser!.email!,
+                        style: TextStyle(fontSize: 17),
+                      ),
+                      const SizedBox(
+                        height: 15,
+                      ),
+                      ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                              backgroundColor: const Color(0xFF9053EB),
+                              shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(15)),
+                              minimumSize: const Size(150, 50)),
+                          onPressed: firebaseLogout,
+                          child: const Text(
+                            'Выйти',
+                            style: TextStyle(fontSize: 17),
+                          )),
+                    ],
+                  )
+                ],
+              ),
             )
-          ],
-        ),
-      ),
+          : const Center(
+              child: CircularProgressIndicator(),
+            ),
     );
   }
 }
