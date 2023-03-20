f90695e4f5737df904099e54e50f2e555637b02f
Vasiliy Prokudin <vd.prokudin@jet.su>
2023-03-20 15:45:06 +0300

    added fromJson and toJson to entities

--- diplom_money_tracker/flutter_diplom_money_tracker/lib/src/data/cost_category.dart
+++ diplom_money_tracker/flutter_diplom_money_tracker/lib/src/data/cost_category.dart
@@ -9,4 +9,21 @@ class CostCategory {
       {required this.categoryName,
       required this.categoryColor,
       this.items = const []});
+
+  factory CostCategory.fromJson(Map<String, Object?> json) => CostCategory(
+        categoryName: json['name'] as String,
+        categoryColor: json['color'] as String,
+        items: json['costs'] != null
+            ? [
+                ...(json['costs'] as List<dynamic>)
+                    .map((item) => CostItem.fromJson(item))
+              ]
+            : [],
+      );
+
+  Map<String, Object?> toJson() => {
+        'name': categoryName,
+        'color': categoryColor,
+        'costs': items.map((e) => e.toJson())
+      };
 }
--- diplom_money_tracker/flutter_diplom_money_tracker/lib/src/data/cost_item.dart
+++ diplom_money_tracker/flutter_diplom_money_tracker/lib/src/data/cost_item.dart
@@ -1,6 +1,16 @@
 class CostItem {
-  final double costPrice;
+  final num costPrice;
   final String costDay;
 
   CostItem({required this.costPrice, required this.costDay});
+
+  factory CostItem.fromJson(Map<String, Object?> json) => CostItem(
+        costDay: json['date'] as String,
+        costPrice: (json['price'] as num),
+      );
+
+  Map<String, Object?> toJson() => {
+        'date': costDay,
+        'price': costPrice,
+      };
 }
