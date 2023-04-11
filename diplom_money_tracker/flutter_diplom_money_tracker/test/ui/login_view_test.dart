07ccec2da114e4849adb003eb4bb1df7a7d8bf1f
Vasiliy <prokudinvasya@gmail.com>
2023-04-11 16:43:46 +0300

    fixed ui test

--- diplom_money_tracker/flutter_diplom_money_tracker/test/ui/login_view_test.dart
+++ diplom_money_tracker/flutter_diplom_money_tracker/test/ui/login_view_test.dart
@@ -1,19 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_diplom_money_tracker/src/ui/login/login_view.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:module_business/module_business.dart';
+import 'package:module_data/module_data.dart';
 
 void main() {
   group('Login view test', () {
     testWidgets('Login form extits', (widgetTester) async {
       await widgetTester.pumpWidget(MaterialApp(
-        home: LoginView(),
+        home: RepositoryProvider(
+            create: (context) => AuthRepository(),
+            child: BlocProvider(
+              create: (context) => SessionCubit(
+                authRepo: context.read<AuthRepository>(),
+              ),
+              child: BlocProvider(
+                create: (context) => AuthCubit(
+                  sessionCubit: context.read<SessionCubit>(),
+                ),
+                child: const LoginView(),
+              ),
+            )),
       ));
 
-      // final fieldFinderEmail = find.text('E-mail');
-      // final fieldFinderSubmit = find.text('Войти');
       final fieldFinderSubmit = find.text('Don\'t have an account? Sign up.');
 
-      // expect(fieldFinderEmail, findsOneWidget);
       expect(fieldFinderSubmit, findsOneWidget);
     });
   });
